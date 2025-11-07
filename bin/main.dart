cc1073df9240d8f4ce1c1241857691305e962ff2
Maria Eduarda <mrodrigues.mariaeduarda@gmail.com>
2025-11-07 14:58:12 -0300

    Enviando arquivos do curso

---
+++ bin/main.dart
@@ -0,0 +1,54 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+void main() {
+  // print("Olá, mundo!");
+  // requestData();
+  // requestDataAsync();
+
+  sendDataAssync({
+    "id": "NEW001",
+    "name": "Flutter",
+    "lastName": "Dart",
+    "balance": 5000,
+  });
+}
+
+requestData() {
+  String url = "https://gist.githubusercontent.com/ricarthlima/a0eb198cb7a70696c4031e7e577de0cd/raw/356ce2c39dfd58d3d2e948d1ad87ea828544f1db/accounts.json";
+  Future<Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
+  futureResponse.then((Response response) {
+      print(response);
+      print(response.body);
+      List<dynamic> listAccounts = json.decode(response.body);
+
+      Map<String, dynamic> mapCarla = listAccounts.firstWhere(
+        (element) => element["name"] == "Carla",
+      );
+
+      print(mapCarla["balance"]);
+    }
+  );
+
+  print("Última coisa a acontecer na função.");
+}
+
+Future<List<dynamic>> requestDataAsync() async {
+  String url = "https://gist.githubusercontent.com/Duda-Martins/ef4e3a9a9f160a2180edbdd71c0ba0a9/raw/13efb40729578124e96ebae30cd6c608805ef478/accounts.json";
+  Response response = await get(Uri.parse(url));
+  
+  return json.decode(response.body);
+}
+
+sendDataAssync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestDataAsync();
+  listAccounts.add(mapAccount);
+  String content = json.encode(listAccounts);
+
+  String url =
+    "https://gist.githubusercontent.com/Duda-Martins/ef4e3a9a9f160a2180edbdd71c0ba0a9/raw/13efb40729578124e96ebae30cd6c608805ef478/accounts.json";
+  
+  Response response = await post(Uri.parse(url), body:content, );
+  print(response.statusCode);
+}

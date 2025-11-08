305b77b95214b8cd9e25dd173fb24e6ae8ba550b
Maria Eduarda <mrodrigues.mariaeduarda@gmail.com>
2025-11-08 10:18:28 -0300

    Forçando um erro e lidando com ele com o try on ClientException e o try on Exception.

--- lib/screens/account_screens.dart
+++ lib/screens/account_screens.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:dart_assincronismo/services/account_service.dart';
+import 'package:http/http.dart';
 
 import '../models/account.dart';
 
@@ -56,8 +57,16 @@ class AccountScreen {
   }
 
   _getAllAccounts() async {
-    List<Account> listAccounts = await _accountService.getAll();
-    print(listAccounts);
+    try {
+      List<Account> listAccounts = await _accountService.getAll();
+      print(listAccounts);
+    } on ClientException {
+      print("Não foi possível alcançar o servidor.");
+      print("Tente novamente mais tarde.");
+    } on Exception {
+      print("Não consegui recuperar os dados da conta.");
+      print("Tente novamente mais tarde.");
+    }
   }
 
   _addExampleAccount() async {

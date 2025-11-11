2b72e98a6134d70ada28bd728a717652843aa74c
Maria Eduarda <mrodrigues.mariaeduarda@gmail.com>
2025-11-11 13:40:55 -0300

    Implementando tsnsações com taxas com null safety.

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,14 @@
 import 'package:dart_assincronismo/screens/account_screens.dart';
+import 'package:dart_assincronismo/services/transaction_service.dart';
 
 void main() {
-  AccountScreen accountScreen = AccountScreen();
-  accountScreen.initializeStream();
-  accountScreen.runChatBot();
+  TransactionService().makeTransaction(
+    idSender: "ID001",
+    idReceiver: "ID003",
+    amount: 50,
+  );
+
+  // AccountScreen accountScreen = AccountScreen();
+  // accountScreen.initializeStream();
+  // accountScreen.runChatBot();
 }

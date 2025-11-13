e5dcefd26751cf78bff556ff61ac65315bdaac12
Maria Eduarda <mrodrigues.mariaeduarda@gmail.com>
2025-11-12 22:53:41 -0300

    Implementando capturas assíncronas de erros personalizados.

--- bin/main.dart
+++ bin/main.dart
@@ -3,18 +3,34 @@ import 'package:dart_assincronismo/screens/account_screens.dart';
 import 'package:dart_assincronismo/services/transaction_service.dart';
 
 void main() {
-  try {
-    TransactionService().makeTransaction(
-        idSender: "ID001",
-        idReceiver: "ID002",
-        amount: 5000,
-    );
-    } on InsufficientFundsException catch (e) {
-        print(e.message);
-        print("${e.cause.name} possui saldo ${e.cause.balance} que é menor que ${e.amount + e.taxes}");
-    }
-
-  // AccountScreen accountScreen = AccountScreen();
-  // accountScreen.initializeStream();
-  // accountScreen.runChatBot();
+  TransactionService().makeTransaction(
+      idSender: "ID001",
+      idReceiver: "ID002",
+      amount: 5000,
+  ).catchError(
+    (e){
+      print(e.message);
+      print("${e.cause.name} possui saldo ${e.cause.balance} que é menor que ${e.amount + e.taxes}");
+    },
+    test: (error) => error is InsufficientFundsException,
+  ).then(
+    (value) {},
+  );
 }
+
+// void main() async {
+//   try {
+//     await TransactionService().makeTransaction(
+//         idSender: "ID001",
+//         idReceiver: "ID002",
+//         amount: 5000,
+//     );
+//     } on InsufficientFundsException catch (e) {
+//         print(e.message);
+//         print("${e.cause.name} possui saldo ${e.cause.balance} que é menor que ${e.amount + e.taxes}");
+//     }
+
+//   // AccountScreen accountScreen = AccountScreen();
+//   // accountScreen.initializeStream();
+//   // accountScreen.runChatBot();
+// }
